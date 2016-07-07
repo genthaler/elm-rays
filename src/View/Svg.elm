7ab07672dd326cbb868a4f625e6b71dffa86196e
Kris Jenkins <krisajenkins@gmail.com>
2016-07-07 14:11:22 +0100

    Refactoring the main SVG view.

--- src/View/Svg.elm
+++ src/View/Svg.elm
@@ -16,67 +16,82 @@ root walls size position =
         [ width "600"
         , height "600"
         ]
-        [ g []
-            (let
-                solutions =
-                    solveRays walls
-                        { x = toFloat position.x
-                        , y = toFloat position.y
-                        }
-                        |> List.sortBy (.vector >> .angle)
-
-                cycled =
-                    solutions ++ (List.take 1 solutions)
-             in
-                List.map2 (,) cycled (List.tail cycled |> Maybe.withDefault [])
-                    |> List.map drawTriangle
-            )
-        , g [] (List.map drawLine walls)
-        , circle
-            [ cx <| toString position.x
-            , cy <| toString position.y
-            , r "5"
-            , fill "red"
-            ]
-            []
+        [ drawRays walls position
+        , drawWalls walls
+        , drawCursor position
         ]
+
+
+neighbours : List a -> List ( a, a )
+neighbours xs =
+    List.map2 (,)
+        xs
+        ((xs ++ xs)
+            |> List.tail
+            |> Maybe.withDefault []
+        )
+
+
+drawRays : Walls -> Mouse.Position -> Svg msg
+drawRays walls position =
+    solveRays walls
+        { x = toFloat position.x
+        , y = toFloat position.y
+        }
+        |> List.sortBy (.vector >> .angle)
+        |> neighbours
+        |> List.map drawTriangle
+        |> g []
+
+
+drawWalls : Walls -> Svg msg
+drawWalls walls =
+    g []
+        (List.map drawLine walls)
 
 
 drawLine : Line -> Svg msg
 drawLine line =
-    let
-        ( startX, startY ) =
-            toXY <| Vectors.start line
-
-        ( endX, endY ) =
-            toXY <| Vectors.end line
-    in
-        Svg.line
-            [ x1 <| toString <| startX
-            , y1 <| toString <| startY
-            , x2 <| toString <| endX
-            , y2 <| toString <| endY
-            , stroke "black"
-            , strokeWidth "5"
-            , strokeLinecap "round"
-            ]
-            []
+    Svg.line
+        [ x1 <| toString <| fst <| toXY <| Vectors.start line
+        , y1 <| toString <| snd <| toXY <| Vectors.start line
+        , x2 <| toString <| fst <| toXY <| Vectors.end line
+        , y2 <| toString <| snd <| toXY <| Vectors.end line
+        , stroke "black"
+        , strokeWidth "5"
+        , strokeLinecap "round"
+        ]
+        []
 
 
 drawTriangle : ( Line, Line ) -> Svg msg
 drawTriangle ( a, b ) =
-    polygon
-        [ fill "#fce64e"
-        , stroke "#fce64e"
-        , points
-            ([ Vectors.start a
-             , Vectors.end a
-             , Vectors.end b
-             , Vectors.start b
-             ]
-                |> List.map toXY
-                |> List.map (\( x, y ) -> toString x ++ "," ++ toString y)
-                |> String.join " "
-            )
+    let
+        toPair ( x, y ) =
+            toString x ++ "," ++ toString y
+    in
+        polygon
+            [ fill "#fce64e"
+            , stroke "#fce64e"
+            , points
+                (String.join " "
+                    <| List.map (toXY >> toPair)
+                    <| [ Vectors.start a
+                       , Vectors.end a
+                       , Vectors.end b
+                       , Vectors.start b
+                       ]
+                )
+            ]
+            []
+
+
+drawCursor : Mouse.Position -> Svg msg
+drawCursor position =
+    circle
+        [ cx <| toString position.x
+        , cy <| toString position.y
+        , r "5"
+        , fill "red"
         ]
         []
